5696f4ba5d32399aa8709f249d269a3ac613304a
Stefan Berger <is191308@fhstp.ac.at>
2021-12-05 11:19:39 +0100

    Inital commit

---
+++ sniffable/src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/SniffableApplication.java
@@ -0,0 +1,13 @@
+package at.fhstp.bis19.prog4.snowdogs.sniffable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SniffableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SniffableApplication.class, args);
+	}
+
+}
---
+++ sniffable/src/test/java/at/fhstp/bis19/prog4/snowdogs/sniffable/SniffableApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhstp.bis19.prog4.snowdogs.sniffable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SniffableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
