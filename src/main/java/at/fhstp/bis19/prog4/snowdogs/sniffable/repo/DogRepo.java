e94256f551b0fabb39457d0234c4a49d4211d297
Stefan Berger <is191308@fhstp.ac.at>
2021-12-06 12:05:47 +0100

    New findByName method

--- src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/repo/DogRepo.java
+++ src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/repo/DogRepo.java
@@ -1,9 +1,11 @@
 package at.fhstp.bis19.prog4.snowdogs.sniffable.repo;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Dog;
 
 @Repository
-public interface DogRepo extends BaseCrudRepository<Dog>{
-
+public interface DogRepo extends BaseCrudRepository<Dog> {
+	public List<Dog> findByNameIgnoreCase(String name);
 }
