2da56dd74f4b2602ff5bc9f05bbcd5b14070ece4
Stefan Berger <is191308@fhstp.ac.at>
2021-12-06 13:55:32 +0100

    Code format

--- src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/controller/ClientController.java
+++ src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/controller/ClientController.java
@@ -2,7 +2,6 @@ package at.fhstp.bis19.prog4.snowdogs.sniffable.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
