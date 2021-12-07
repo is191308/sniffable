1f347f3eea1476215555845ae9284eb3d91e0764
is191320 <is191320@fhstp.ac.at>
2021-12-07 13:12:25 +0100

    added "restapi/pubdate/addPubdate "POST Method" for

--- src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/controller/DogController.java
+++ src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/controller/DogController.java
@@ -1,12 +1,10 @@
 package at.fhstp.bis19.prog4.snowdogs.sniffable.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Dog;
-import at.fhstp.bis19.prog4.snowdogs.sniffable.service.ClientDogService;
+
 
 @RestController("DogController")
 @RequestMapping("/restapi/dog")
--- src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/service/ClientDogService.java
+++ src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/service/ClientDogService.java
@@ -3,9 +3,8 @@ package at.fhstp.bis19.prog4.snowdogs.sniffable.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+
 
 import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Dog;
 import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Pubdate;
@@ -54,7 +53,7 @@ public class ClientDogService {
 		try {
 			dogID = dogRepo.findByNameIgnoreCase(dogname).get(0).getId();
 		} catch (IndexOutOfBoundsException e) {
-			log.info("Unable to find dogname \"{}\" does not exist!", dogname);
+			log.info("Unable to find dogname,  \"{}\" does not exist!", dogname);
 			return null;
 		}
 				
