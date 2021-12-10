015994af72a831dd698f6e01340258a1600e8653
Stefan Berger <is191308@fhstp.ac.at>
2021-12-10 14:34:04 +0100

    Lombok implementation

--- src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/dto/ImageDTO.java
+++ src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/dto/ImageDTO.java
@@ -1,42 +1,17 @@
 package at.fhstp.bis19.prog4.snowdogs.sniffable.dto;
 
 import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Image;
+import lombok.Data;
 
+@Data
 public class ImageDTO {
 	private int id;
 	private String name;
 	private byte[] imageData;
-	
-	public ImageDTO() {	
-	}
 	
 	public ImageDTO(Image image) {	
 		this.id = image.getId();
 		this.name = image.getName();
 		this.imageData = image.getImageData();
 	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public byte[] getImageData() {
-		return imageData;
-	}
-	public void setImageData(byte[] imageData) {
-		this.imageData = imageData;
-	}
-	@Override
-	public String toString() {
-		return "ImageDTO [id=" + id + ", name=" + name + "]";
-	}
-
 }
--- src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/dto/PubdateDTO.java
+++ src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/dto/PubdateDTO.java
@@ -3,7 +3,9 @@ package at.fhstp.bis19.prog4.snowdogs.sniffable.dto;
 import java.util.Date;
 
 import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Pubdate;
+import lombok.Data;
 
+@Data
 public class PubdateDTO implements Comparable<PubdateDTO> {
 	private int id;
 	private String title;
@@ -11,9 +13,6 @@ public class PubdateDTO implements Comparable<PubdateDTO> {
 	private String content;
 	private ImageDTO picture;
 	private DogDTO dog;
-	
-	public PubdateDTO() {
-	}
 	
 	public PubdateDTO(Pubdate pubdate) {
 		this.id = pubdate.getId();
@@ -24,48 +23,6 @@ public class PubdateDTO implements Comparable<PubdateDTO> {
 		this.dog = new DogDTO(pubdate.getDog());
 	}
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public Date getTimestamp() {
-		return timestamp;
-	}
-	public void setTimestamp(Date timestamp) {
-		this.timestamp = timestamp;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public DogDTO getDog() {
-		return dog;
-	}
-	public void setDog(DogDTO dog) {
-		this.dog = dog;
-	}
-	public ImageDTO getPicture() {
-		return picture;
-	}
-	public void setPicture(ImageDTO picture) {
-		this.picture = picture;
-	}
-	
-	@Override
-	public String toString() {
-		return "PubdateDTO [id=" + id + ", title=" + title + ", timestamp=" + timestamp + "]";
-	}
-
 	@Override
 	public int compareTo(PubdateDTO o) {
 		return this.timestamp.compareTo(o.timestamp);
