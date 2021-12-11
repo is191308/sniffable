235a0d6c716f251504c0e07f898d25384cc74b12
Stefan Berger <is191308@fhstp.ac.at>
2021-12-11 19:28:10 +0100

    timestamp removed

--- src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/dto/NewPubdateDTO.java
+++ src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/dto/NewPubdateDTO.java
@@ -1,16 +1,13 @@
 package at.fhstp.bis19.prog4.snowdogs.sniffable.dto;
 
-import java.util.Date;
-
 import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Pubdate;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class NewPubdateDTO implements Comparable<NewPubdateDTO> {
+public class NewPubdateDTO {
 	private String title;
-	private Date timestamp;
 	private String content;
 	private ImageDTO picture;
 	private DogDTO dog;
@@ -18,18 +15,12 @@ public class NewPubdateDTO implements Comparable<NewPubdateDTO> {
 	public NewPubdateDTO(Pubdate pubdate) {
 		if (pubdate != null) {
 			this.title = pubdate.getTitle();
-			this.timestamp = pubdate.getTimestamp();
 			if (pubdate.getPicture() != null) {
 				this.picture = new ImageDTO(pubdate.getPicture());
 			}
 			this.content = pubdate.getContent();
 			this.dog = new DogDTO(pubdate.getDog());
 		}
-	}
-
-	@Override
-	public int compareTo(NewPubdateDTO o) {
-		return this.timestamp.compareTo(o.timestamp);
 	}
 
 }
