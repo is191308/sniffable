663ac24cb5387c36f1e73a54056ab9529dceaa09
Stefan Berger <is191308@fhstp.ac.at>
2021-12-12 18:42:55 +0100

    removed unused imports

--- src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/controller/CommentController.java
+++ src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/controller/CommentController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import at.fhstp.bis19.prog4.snowdogs.sniffable.dto.CommentDto;
 import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Comment;
-import at.fhstp.bis19.prog4.snowdogs.sniffable.service.BaseService;
 import at.fhstp.bis19.prog4.snowdogs.sniffable.service.CommentService;
 
 @RestController
