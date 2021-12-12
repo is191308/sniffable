f518b608e3a28ee7c0cd4e81a47f8e6b5f298f7d
Stefan Berger <is191308@fhstp.ac.at>
2021-12-12 19:41:13 +0100

    Add Dog filed required

--- src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/dto/NewPubdateDto.java
+++ src/main/java/at/fhstp/bis19/prog4/snowdogs/sniffable/dto/NewPubdateDto.java
@@ -1,6 +1,7 @@
 package at.fhstp.bis19.prog4.snowdogs.sniffable.dto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,4 +14,6 @@ public class NewPubdateDto implements BaseDto {
 	@NotBlank
 	private String content;
 	private ImageDto picture;
+	@NotNull
+	private DogDto dog;
 }
