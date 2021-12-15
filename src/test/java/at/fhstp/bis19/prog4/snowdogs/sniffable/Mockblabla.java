66e8f0f0d765ed5eb95a15853b12b4cf2d9e04b3
is191320 <is191320@fhstp.ac.at>
2021-12-15 15:18:49 +0100

    mockito try - funktioniert noch nicht!

--- src/test/java/at/fhstp/bis19/prog4/snowdogs/sniffable/Mockblabla.java
+++ src/test/java/at/fhstp/bis19/prog4/snowdogs/sniffable/Mockblabla.java
@@ -1,45 +1,63 @@
-//package at.fhstp.bis19.prog4.snowdogs.sniffable;
-//
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//import org.modelmapper.ModelMapper;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//
-//import at.fhstp.bis19.prog4.snowdogs.sniffable.dto.DogDto;
-//import at.fhstp.bis19.prog4.snowdogs.sniffable.dto.NewDogDto;
-//import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Dog;
-//import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Dog.Role;
-//import at.fhstp.bis19.prog4.snowdogs.sniffable.service.DogService;
-//
-//@SpringBootTest
-//public class Mockblabla {
-//
-//	private static ModelMapper mapper;
-//
-//	Dog d1 = Dog.builder().name("Testdog").password("testpw").role(Role.USER).build();
-//	NewDogDto nd1 = mapper.map(d1, NewDogDto.class);
-//	
-//	Dog d2 = Dog.builder().name("Testdog").password("testpw").role(Role.USER).build();
-//	NewDogDto nd2 = mapper.map(d2, NewDogDto.class);
-//	
-//	
-//	
-//	@BeforeAll
-//	private static void createMapper() {
-//		mapper = new ModelMapper();
-//	}
-//	
-//	
-//	  @MockBean
-//	  private DogService dogServiceMock;
-//	
-//	
-//	  
-//	  @Test
-//	  void testSomething() {
-//		  Mockito.when(dogServiceMock.createDog(nd1)).thenReturn(mapper.map(nd1, DogDto.class));  
-//		  System.out.println(dogServiceMock.createDog(nd1));
-//	  }
-//}
+package at.fhstp.bis19.prog4.snowdogs.sniffable;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import at.fhstp.bis19.prog4.snowdogs.sniffable.dto.NewPubdateDto;
+import at.fhstp.bis19.prog4.snowdogs.sniffable.dto.PubdateDto;
+import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Dog;
+import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Dog.Role;
+import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Image;
+import at.fhstp.bis19.prog4.snowdogs.sniffable.entity.Pubdate;
+import at.fhstp.bis19.prog4.snowdogs.sniffable.service.PubdateService;
+
+@SpringBootTest
+public class Mockblabla {
+
+	@MockBean
+	private PubdateService pubdateServiceMock;
+
+	
+	private static ModelMapper mapper;
+	
+	//craft NewPubdate for given Object Test
+	Dog d1 = Dog.builder().name("TestDog").password("testpw").role(Role.USER).build();
+	Image i1 = Image.builder().name("Pic1").imageData("asdfadfasfasf".getBytes()).build();
+	Pubdate p1 = Pubdate.builder().title("testTitle").content("testContent").dog(d1).picture(i1).build();
+	NewPubdateDto np1 = mapper.map(p1, NewPubdateDto.class);
+
+	
+	@BeforeAll
+	private static void createMapper() {
+		mapper = new ModelMapper();
+	}
+	
+	
+
+	
+	  
+	  @Test
+	  void testSomething() {
+		  //prepare	-> return PubdateDto
+		  Mockito.when(pubdateServiceMock.createPubdate(np1)).thenReturn(mapper.map(np1, PubdateDto.class));  
+		  
+		  //test correct transition from given NewPubdateDto to PubdateDto
+		  assertFalse(pubdateServiceMock.createPubdate(np1).getClass().equals(np1.getClass()));
+
+		  //verify method only called once
+		  verify(pubdateServiceMock, times(1)).createPubdate(np1);
+		  
+		  //somehow... timestamp is null?!   -> wollte eigentlich checken ob er gesetzt wurde
+		  System.out.println(pubdateServiceMock.createPubdate(np1).getTimestamp());
+		  
+	  }
+}
